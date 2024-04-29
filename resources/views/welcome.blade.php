a4f4d46636c6b4fa18430a06916813b21d4f9125
Davide Cutrona <d.cutrona85@gmail.com>
2024-04-29 23:23:36 +0200

    Carousel completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<x-layout title="Homepage Nintendo"/>
+<x-layout title="Homepage Nintendo" />
 <!--Header Homepage-->
 <header class="container-fluid homepage d-flex justify-content-end">
     <div class="row">
@@ -8,6 +8,50 @@
     </div>
 </header>
 <!--Fine Header Homepage-->
+
+<!--Carousel-->
+<div class="container my-5">
+    <div class="row py-5 bg-danger">
+        <div class="col-12 bg-danger">
+            <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="5000">
+                        <img src="./media/nintendo.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-md-block">
+                            <h5 class="text-white fs-1 carousel-item-fontcustom">NEXT & OLD</h5>
+                            <p class="text-white fs-5">Dalle console più recenti fino a quelle più vecchie, più di 40 anni di successi</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item" data-bs-interval="4000">
+                        <img src="./media/nes.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-md-block">
+                            <h5 class="text-white fs-1 carousel-item-fontcustom">NES STYLE??</h5>
+                            <p class="text-white fs-5">Scopri o rivivi le emozioni di una volta se sei un nostalgico</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item" data-bs-interval="4000">
+                        <img src="./media/personaggi.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-md-block">
+                            <h5 class="text-white fs-1 carousel-item-fontcustom">COMMUNITY</h5>
+                            <p class="fs-4 rounded transparent-blur">Sfoglia il catalogo degli altri giocatori per vedere cosa ti manca!</p>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Fine Carousel-->
 
 <!--Footer-->
 <x-footer />
